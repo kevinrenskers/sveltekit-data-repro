c01690421ec0c24648986302a8cd8c5dde86422b
Kevin Renskers <kevin@loopwerk.io>
2022-08-26 17:15:03 +0200

    Fix comment

--- src/routes/route/subroute/+layout.ts
+++ src/routes/route/subroute/+layout.ts
@@ -3,7 +3,8 @@ import type { LayoutLoad } from "./$types";
 export const load: LayoutLoad = async ({ parent }) => {
   const data = await parent();
 
-  console.log("/route/+page.ts", data.foo);
+  console.log("/route/subrout/+layout.ts", data.foo);
+  console.log("/route/subrout/+layout.ts", data.xyz);
 
   return {
     ...data,
