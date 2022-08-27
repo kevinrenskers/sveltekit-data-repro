1f902b44a7f45be811a3c3508bc915aa114b5362
Kevin Renskers <kevin@loopwerk.io>
2022-08-27 09:35:46 +0200

    Add layout files

---
+++ src/routes/route/subroute/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  export let data: PageData;
+
+  console.log("/route/subroute/+page.svelte", data.foo);
+  console.log("/route/subroute/+page.svelte", data.xyz);
+</script>
+
+Hello route
